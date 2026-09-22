593ed0a7af1f4911bc59b322605a26871a462c9d
smartandjai <smartandjiatechnologies@gmail.com>
2026-09-23 00:19:26 +0200

    fix(compiler): remove duplicate Icons import in NkyelChatScreen

--- app/src/main/java/com/smartandj/gabomagpt/presentation/chat/NkyelChatScreen.kt
+++ app/src/main/java/com/smartandj/gabomagpt/presentation/chat/NkyelChatScreen.kt
@@ -23,8 +23,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.ui.res.painterResource
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import com.smartandj.gabomagpt.R
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
